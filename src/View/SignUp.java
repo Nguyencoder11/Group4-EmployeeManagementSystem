0588a6f7668ffa41c201b65b522d63a5bbc0fecd
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-05-31 09:25:06 +0700

    update for Admin and Signup

--- src/View/SignUp.java
+++ src/View/SignUp.java
@@ -208,7 +208,7 @@ public class SignUp extends javax.swing.JFrame {
             // lay thong tin nguoi dung
             String username = txtUser.getText().trim();
             String password = new String(txtPassword.getPassword()).trim();
-            int userType = 1;   // set mac dinh tk admin
+            int userType = 1;   // set mac dinh tk employee
 
             // neu dang ky username co chua tu "admin" thi la tai khoan admin
             if (username.toLowerCase().contains("admin")) {
